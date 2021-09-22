a568ac64cb7928704f2f2ea3d252b27cfc60ce2e
GiBetanzor <87739477+GiBetanzor@users.noreply.github.com>
2021-09-22 15:34:33 +0800

    index
    Added the Index and related sass and images (navbar & footer)

---
+++ index.php
@@ -0,0 +1,161 @@
+<!doctype html>
+<html>
+<head>
+    <title>Assist</title>
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href=".//css/Styles.css">
+</head>
+
+<body>
+
+<!--Navigation Bar-->
+<nav>
+    <div class="innerNav">
+        <img src="./images/Assist2_NavBar.png" class="navbarBrand">
+        <a href="#login">Login</a>
+        <a href="#signup">Signup</a>
+        <a href="#serviceProvider">Service Provider</a>
+    </div>
+</nav>
+
+<div class="pageTopSpacer"></div>
+
+<!--Page Content-->
+This is the body!
+<br>This is the body!
+<br>This is the body!
+<br>This is the body!
+This is the body!
+<br>This is the body!
+<br>This is the body!
+<br>This is the body!
+This is the body!
+<br>This is the body!
+<br>This is the body!
+<br>This is the body!
+
+
+<!--Footer-->
+<footer>
+    <div class="footerServices">
+        <div class="footerservicesColumn">
+            <h4>CLEANING & MAINTENANCE</h4>
+            <ul class="footerServicesBullets">
+                <li><a class="footerServicesLink" href="#">Aircon cleaning</a></li>
+                <li><a class="footerServicesLink" href="#">Area disinfection</a></li>
+                <li><a class="footerServicesLink" href="#">Gardening</a></li>
+                <li><a class="footerServicesLink" href="#">Housekeeping</a></li>
+                <li><a class="footerServicesLink" href="#">Ironing</a></li>
+                <li><a class="footerServicesLink" href="#">Laundry</a></li>
+                <li><a class="footerServicesLink" href="#">Pest extermination</a></li>
+            </ul>
+        </div>
+        <div class="footerservicesColumn">
+            <h4>CONSTRUCTION</h4>
+            <ul class="footerServicesBullets">
+                <li><a class="footerServicesLink" href="#">Carpentry</a></li>
+                <li><a class="footerServicesLink" href="#">Paint works</a></li>
+                <li><a class="footerServicesLink" href="#">Plumbing</a></li>
+            </ul>
+        </div>
+        <div class="footerservicesColumn">
+            <h4>COMPUTER REPAIR& MAINTENANCE</h4>
+            <ul class="footerServicesBullets">
+                <li><a class="footerServicesLink" href="#">Reformat</a></li>
+            </ul>
+        </div>
+        <div class="footerservicesColumn">
+            <h4>APPLIANCE REPAIR</h4>
+            <ul class="footerServicesBullets">
+                <li><a class="footerServicesLink" href="#">Electric fan</a></li>
+                <li><a class="footerServicesLink" href="#">TV</a></li>
+                <li><a class="footerServicesLink" href="#">Washing machine</a></li>
+            </ul>
+        </div>
+        <div class="footerservicesColumn">
+            <h4>EVENTS</h4>
+            <ul class="footerServicesBullets">
+                <li><a class="footerServicesLink" href="#">Photography</a></li>
+                <li><a class="footerServicesLink" href="#">Hosting / Emcee</a></li>
+            </ul>
+        </div>
+        <div class="footerservicesColumn">
+            <h4>TRANSPORT</h4>
+            <ul class="footerServicesBullets">
+                <li><a class="footerServicesLink" href="#">Driver for hire</a></li>
+                <li><a class="footerServicesLink" href="#">Driving lesson</a></li>
+                <li><a class="footerServicesLink" href="#">Rent a vehicle</a></li>
+            </ul>
+        </div>
+        <div class="footerservicesColumn">
+            <h4>VEHICLE REPAIR</h4>
+            <ul class="footerServicesBullets">
+                <li><a class="footerServicesLink" href="#">Engine overhaul</a></li>
+                <li><a class="footerServicesLink" href="#">Change oil</a></li>
+            </ul>
+        </div>
+        <div class="footerservicesColumn">
+            <h4>WELLNESS & PERSONAL GROOMING</h4>
+            <ul class="footerServicesBullets">
+                <li><a class="footerServicesLink" href="#">Footspa</a></li>
+                <li><a class="footerServicesLink" href="#">Haircut</a></li>
+                <li><a class="footerServicesLink" href="#">Hair treatment</a></li>
+                <li><a class="footerServicesLink" href="#">Hair dressing & Make-up</a></li>
+                <li><a class="footerServicesLink" href="#">Manicure & Pedicure/a></li>
+                <li><a class="footerServicesLink" href="#">Massage</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="footerBottom">
+        <div class="footerBottomColumn">
+            <h4>ABOUT ASSIST</h4>
+            <ul class="footerBottomBullets">
+                <li><a class="footerBottomLink" href="#">How Assist Works</a></li>
+                <li><a class="footerBottomLink" href="#">Affiliate Program</a></li>
+                <li><a class="footerBottomLink" href="#">Work Opportunities</a></li>
+            </ul>
+        </div>
+        <div class="footerBottomColumn">
+            <h4>CUSTOMER CARE</h4>
+            <ul class="footerBottomBullets">
+                <li><a class="footerBottomLink" href="#">How to book a service?</a></li>
+                <li><a class="footerBottomLink" href="#">Assist Guarantee</a></li>
+                <li><a class="footerBottomLink" href="#">Payment Methods</a></li>
+                <li><a class="footerBottomLink" href="#">Contact Us</a></li>
+            </ul>
+        </div>
+        <div class="footerBottomColumn">
+            <h4>COLLABORATE</h4>
+            <ul class="footerBottomBullets">
+                <li><a class="footerBottomLink" href="#">How to list a service?</a></li>
+                <li><a class="footerBottomLink" href="#">Be a Service Provider</a></li>
+                <li><a class="footerBottomLink" href="#">Be an affiliate</a></li>
+            </ul>
+        </div>
+        <div class="footerBottomColumn">
+            <h4>CONTACT US</h4>
+            <ul class="footerBottomBullets">
+                <li><img src="./images/Icon_Email.png"><a class="footerBottomLink" href="#">info@assist.ph</a></li>
+                <li><img src="./images/Icon_Phone.png">09981923360</li>
+                <li><img src="./images/Icon_Map.png">2601 Rockefeller St.,
+                    <br>San Isidro
+                    <br>Makati City
+                </li>
+            </ul>
+        </div>
+        <div class="footerBottomColumn">
+            <h4>STAY CONNECTED</h4>
+            <ul class="footerBottomBullets">
+                <li><img src=".//images/Logo_Facebook.png"><a class="footerBottomLink" href="#">Facebook</a></li>
+                <li><img src="./images/Logo_Instagram.png"><a class="footerBottomLink" href="#">Instagram</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="footerEnd">
+        <hr class="footerHR">
+        <p class="footerParagraph">&copy; Assist 2021 | All rights reserved | <span class="footerEndLink">Terms of Use</span> | <span class="footerEndLink">Privacy Policy</span></p>
+    </div>
+</footer>
+
+</body>
+</html>
